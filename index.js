3938f777a593079f519ea6b24b81438878a91579
Raphael Chaula <raphaelchaula@gmail.com>
2020-10-18 17:38:30 +0300

    Inirial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const bernOne = () => {
+    console.log("I am Bern");
+};
+
+const bernTwo = () => {
+    console.log("I work at Nunu");
+};
+
+const bernThree = () => {
+    console.log("So I am @nunu/bern");
+};
+
+module.exports = {
+    bernOne,
+    bernTwo,
+    bernThree
+}
